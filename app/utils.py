c363d243d10068522eac672ff996f228e7adfca4
buivanduc3031 <127102693+buivanduc3031@users.noreply.github.com>
2024-12-15 12:40:53 +0700

    admin update

---
+++ app/utils.py
@@ -0,0 +1,6 @@
+def cart_stats(cart):
+    # total_quantity = sum(item['quantity'] for item in cart.values())
+    # total_price = sum(item['quantity'] * item['price'] for item in cart.values())
+    total_quantity = 10
+    total_price = 10
+    return {'total_quantity': total_quantity, 'total_price': total_price}
